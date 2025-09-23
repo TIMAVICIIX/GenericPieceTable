509d2aa5fe939626c15405344e9a3cfe6215fc92
TIMAVICIIX <104446598+TIMAVICIIX@users.noreply.github.com>
2025-09-23 16:54:09 +0800

    Realize the max stack history function.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 import org.jreleaser.model.Active
-import org.jreleaser.model.Signing
 
 plugins {
     kotlin("jvm") version "1.9.22"
     `java-library`
     `maven-publish`
-    id("org.jreleaser") version "1.13.1"
+    id("org.jreleaser") version "1.18.0"
 }
 
 group = "com.ebmlibs"
@@ -23,6 +22,16 @@ tasks.register<Jar>("javadocJar") {
     archiveClassifier.set("javadoc")
     from(tasks.javadoc)
 }
+//javadoc，如果用jdk11，默认就支持中文
+//查看可以配置的属性：https://docs.gradle.org/current/javadoc/org/gradle/external/javadoc/StandardJavadocDocletOptions.html
+tasks.withType<Javadoc>().configureEach {
+    options.encoding = "UTF-8"
+    if (options is StandardJavadocDocletOptions) {
+        (options as StandardJavadocDocletOptions).charSet = "UTF-8" // 解决中文乱码
+    }
+}
+
+
 
 
 publishing {
@@ -39,62 +48,61 @@ publishing {
                 usage("java-runtime") {
                     fromResolutionResult()
                 }
+            }
 
-                pom {
-                    name.set("GenericPieceTable")
-                    description.set("GenericPieceTable is a library that provides a piece table data structure for efficient text editing.")
+            pom {
+                name.set("GenericPieceTable")
+                description.set("GenericPieceTable is a library that provides a piece table data structure for efficient text editing.")
+                url.set("https://github.com/timaviciix/GenericPieceTable")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("TIMAVICIIX")
+                        name.set("TIMAVICIIX")
+                        email.set("timaviciix@outlook.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/timaviciix/GenericPieceTable.git")
+                    developerConnection.set("scm:git:ssh://github.com/timaviciix/GenericPieceTable.git")
                     url.set("https://github.com/timaviciix/GenericPieceTable")
-                    licenses {
-                        license {
-                            name.set("MIT")
-                            url.set("https://opensource.org/licenses/MIT")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("TIMAVICIIX")
-                            name.set("TIMAVICIIX")
-                            email.set("timaviciix@outlook.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com/timaviciix/GenericPieceTable.git")
-                        developerConnection.set("scm:git:ssh://github.com/timaviciix/GenericPieceTable.git")
-                        url.set("https://github.com/timaviciix/GenericPieceTable")
-                    }
                 }
             }
         }
     }
-    repositories {
-        mavenLocal()
-    }
+}
+repositories {
+    mavenLocal()
 }
 
-//jreleaser {
-//    signing {
-//        active.set(Active.ALWAYS)
-//        armored.set(true)
-//        mode.set(org.jreleaser.model.Signing.Mode.FILE)
-//        publicKey.set("D:\\Develop_Space\\Key\\public.pgp")
-//        secretKey.set("D:\\Develop_Space\\Key\\private.pgp")
-//        passphrase.set(System.getenv("JRELEASER_GPG_PASSPHRASE"))
-//    }
-//
-//    deploy {
-//        maven {
-//            mavenCentral {
-//                create("sonatype") {
-//                    username.set(System.getenv("MAVENCENTRAL_USERNAME"))
-//                    password.set(System.getenv("MAVENCENTRAL_PASSWORD"))
-//                    active.set(Active.ALWAYS)
-//                    url.set("https://central.sonatype.com/api/v1/publisher")
-//                    stagingRepository(System.getProperty("user.home") + "/.m2/repository")
-//                }
-//            }
-//        }
-//    }
-//}
+
+jreleaser {
+    strict.set(false)
+    signing {
+        active.set(Active.ALWAYS)
+        armored.set(true)
+        mode.set(org.jreleaser.model.Signing.Mode.FILE)
+        publicKey.set("D:\\Develop_Space\\Key\\GPG\\public.pgp")
+        secretKey.set("D:\\Develop_Space\\Key\\GPG\\private.pgp")
+    }
+
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active.set(Active.ALWAYS)
+                    url.set("https://central.sonatype.com/api/v1/publisher")
+                    stagingRepository("build/staging-deploy")
+                }
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
